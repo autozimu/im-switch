4b46f7704857fd0933ad87574bbd15ba9321ed3a
Junfeng Li <junfengli@meta.com>
2024-09-07 15:36:30 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -2,22 +2,27 @@
   description = "Flake for im-switch";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "im-switch";
 
-      shell = { pkgs }: pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustup
-        ] ++ lib.optionals stdenvNoCC.isDarwin [
-          libiconv
-          darwin.apple_sdk.frameworks.Carbon
-        ];
-      };
+      shell =
+        { pkgs }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            darwin.apple_sdk.frameworks.Carbon
+          ];
+        };
     };
 }
